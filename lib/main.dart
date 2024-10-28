c9f442863f1e2b39a6fd6049b4801227c719e9d9
robin <robinhartmann@live.com>
2024-10-29 00:44:59 +0100

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
 import 'screens/main_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+	try {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+		print('Firebase is working');
+	} catch (e) {
+		print('Firebase initialization error: $e');
+	}
   runApp(MyApp());
 }
 
