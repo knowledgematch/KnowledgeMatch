eb61ef4ca98f464fada06d697f41afdf17dd4ca3
robin <robinhartmann@live.com>
2024-10-29 01:14:01 +0100

    flake artifacts for @Robin

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Flutter 3.13.x";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = { self, nixpkgs, flake-utils }:
