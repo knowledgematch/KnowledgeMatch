fc34efbbc1ded68cf9eb29edc25a4a2dfe2bb0d2
Daniel <daniel.zimmerli@students.fhnw.ch>
2024-11-27 16:10:57 +0100

    fixed NotificationData factories

--- lib/model/notification_data.dart
+++ lib/model/notification_data.dart
@@ -47,25 +47,26 @@ class NotificationData {
   });
 
   factory NotificationData.fromFirestoreData(Map<String, dynamic> map){
-      Timestamp fireStoreTimestamp = map['timestamp'];
-      return NotificationData(
-        type: NotificationType.fromString(map['notificationType'] ?? ''),
+      String fireStoreTimestamp = map['timestamp'];
+    return NotificationData(
+        type: NotificationType.fromString(map['notification_type'] ?? ''),
         title: map['title'] ?? '',
         body: map['body'] ?? '',
-        targetUserId: int.parse(map['targetUserId'] ?? ''),
-        sourceUserId: int.parse(map['sourceUserId'] ?? ''),
-        timestamp: fireStoreTimestamp.toDate()
+        targetUserId: int.parse(map['target_user_id'] ?? ''),
+        sourceUserId: int.parse(map['source_user_id'] ?? ''),
+        timestamp: DateTime.parse(fireStoreTimestamp)
       );
   }
+
   factory NotificationData.fromMessage(RemoteMessage message){
-    Timestamp fireStoreTimestamp = message.data['timestamp'];
+    String fireStoreTimestamp = message.data['timestamp'];
     return NotificationData(
         title: message.notification?.title ?? '',
         body: message.notification?.body ?? '',
         targetUserId: int.tryParse(message.data['target_user_id']) ?? 0,
         sourceUserId: int.tryParse(message.data['source_user_id']) ?? 0,
         type: NotificationType.fromString(message.data['notification_type']),
-        timestamp: fireStoreTimestamp.toDate()
+        timestamp: DateTime.parse(fireStoreTimestamp)
     );
   }
 }
