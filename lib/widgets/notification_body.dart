de5adb2419234583779e7f1f88884718d1387f93
robin <robinhartmann@live.com>
2024-12-25 14:33:41 +0100

    Change display of request messages
    Change message body of swipe screen

--- lib/widgets/notification_body.dart
+++ lib/widgets/notification_body.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:knowledgematch/model/reachability.dart';
 import 'package:knowledgematch/model/search_criteria.dart';
