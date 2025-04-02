ee79fbf1f9c429d83944c95bd24c0b8286b4e634
Baris Tulgar <baris.tulgar@students.fhnw.ch>
2025-04-02 12:39:12 +0200

    implement design and themeing (add logos and pictures)

---
+++ lib/theme/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppConstants {
+  static BorderRadius borderRadius = BorderRadius.circular(10);
+  static BorderRadius borderRadiusLarge = BorderRadius.circular(12);
+}
