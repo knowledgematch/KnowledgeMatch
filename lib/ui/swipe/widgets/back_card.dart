c45083ae06652fe69a2211b2dd366d6cd0b7dc24
robin <robinhartmann@live.com>
2025-05-08 22:41:21 +0200

    User Story: UI/UX Refinement
    Adjust layout of swipe_screen and card

--- lib/ui/swipe/widgets/back_card.dart
+++ lib/ui/swipe/widgets/back_card.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:knowledgematch/ui/core/themes/app_constants.dart';
 
 import '../../../domain/models/userprofile.dart';
 import '../../core/themes/app_colors.dart';
-import '../../core/themes/app_constants.dart';
-import 'flip_card.dart';
 
 class BackCard extends StatelessWidget {
   final Userprofile profile;
@@ -23,7 +22,7 @@ class BackCard extends StatelessWidget {
     return Container(
       width: width,
       height: height,
-      padding: const EdgeInsets.all(16),
+      // padding: const EdgeInsets.all(16),
       decoration: BoxDecoration(
         color: AppColors.grey2Light,
         borderRadius: BorderRadius.circular(16),
@@ -38,29 +37,25 @@ class BackCard extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          const SizedBox(height: 50),
           Container(
             height: 450,
             width: 380,
             decoration: BoxDecoration(
               color: AppColors.greyShadowLight,
               borderRadius: BorderRadius.circular(20),
-              border: Border.all(
-                color: AppColors.greyShadowLight,
-                width: 2,
-              ),
+              border: Border.all(color: AppColors.greyShadowLight, width: 2),
             ),
             child: ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: profilePicture != null
-                  ? Image.memory(
-                      profilePicture,
-                      fit: BoxFit.cover,
-                    )
-                  : Image.asset(
-                      'assets/images/profile.png',
-                      fit: BoxFit.cover,
-                    ),
+              borderRadius: AppConstants.borderRadiusLarge,
+              child: Flexible(
+                child:
+                    profilePicture != null
+                        ? Image.memory(profilePicture, fit: BoxFit.cover)
+                        : Image.asset(
+                          'assets/images/profile.png',
+                          fit: BoxFit.cover,
+                        ),
+              ),
             ),
           ),
 
@@ -77,70 +72,21 @@ class BackCard extends StatelessWidget {
           ),
           const SizedBox(height: 8),
 
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-            decoration: BoxDecoration(
-              color: AppColors.grey3Light,
-              borderRadius: BorderRadius.circular(8),
-            ),
-            child: const Text(
-              "No qualifications are provided about this expert yet",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 14,
-                color: AppColors.black87Light,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-          ),
-          const SizedBox(height: 16),
-
           Expanded(
             child: Text(
               profile.description == "null"
-                  ? "More information about this person will be available soon."
+                  ? "No information available."
                   : profile.description,
               textAlign: TextAlign.center,
-              style: const TextStyle(
-                fontSize: 16,
-                color: AppColors.greyLight,
-              ),
+              style: const TextStyle(fontSize: 16, color: AppColors.greyLight),
             ),
           ),
 
-          SizedBox(
-            width: double.infinity,
-            child: ElevatedButton.icon(
-              onPressed: () {
-                final flipCardState =
-                    context.findAncestorStateOfType<FlipCardState>();
-                if (flipCardState != null) {
-                  flipCardState.toggleCard();
-                }
-              },
-              icon: const Icon(
-                Icons.touch_app,
-                color: AppColors.whiteLight,
-              ),
-              label: const Text("See detailed information"),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: AppColors.primary,
-                foregroundColor: AppColors.whiteLight,
-                shape: RoundedRectangleBorder(
-                  borderRadius: AppConstants.borderRadiusLarge,
-                ),
-                padding: const EdgeInsets.symmetric(vertical: 12),
-              ),
-            ),
-          ),
           const Padding(
             padding: EdgeInsets.only(top: 8),
             child: Text(
-              "Or simply click anywhere on the page!",
-              style: TextStyle(
-                fontSize: 12,
-                color: AppColors.greyLight,
-              ),
+              "Click anywhere on the page to find out more!",
+              style: TextStyle(fontSize: 12, color: AppColors.greyLight),
               textAlign: TextAlign.center,
             ),
           ),
@@ -153,9 +99,11 @@ class BackCard extends StatelessWidget {
 class RotationYTransition extends AnimatedWidget {
   final Widget child;
 
-  const RotationYTransition(
-      {super.key, required Animation<double> turns, required this.child})
-      : super(listenable: turns);
+  const RotationYTransition({
+    super.key,
+    required Animation<double> turns,
+    required this.child,
+  }) : super(listenable: turns);
 
   @override
   Widget build(BuildContext context) {
@@ -163,13 +111,14 @@ class RotationYTransition extends AnimatedWidget {
     return Transform(
       transform: Matrix4.rotationY(animation.value * 3.1416),
       alignment: Alignment.center,
-      child: animation.value < 0.5
-          ? child
-          : Transform(
-              transform: Matrix4.rotationY(3.1416),
-              alignment: Alignment.center,
-              child: child,
-            ),
+      child:
+          animation.value < 0.5
+              ? child
+              : Transform(
+                transform: Matrix4.rotationY(3.1416),
+                alignment: Alignment.center,
+                child: child,
+              ),
     );
   }
 }
--- lib/ui/swipe/widgets/swipe_screen.dart
+++ lib/ui/swipe/widgets/swipe_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:knowledgematch/domain/models/userprofile.dart';
+import 'package:knowledgematch/ui/core/themes/app_constants.dart';
 import 'package:knowledgematch/ui/swipe/view_model/swipe_view_model.dart';
 import 'package:provider/provider.dart';
 import 'package:swipable_stack/swipable_stack.dart';
@@ -7,128 +8,138 @@ import 'package:swipable_stack/swipable_stack.dart';
 import 'flip_card.dart';
 
 class SwipeScreen extends StatefulWidget {
-
-  const SwipeScreen({
-    super.key,
-  });
+  const SwipeScreen({super.key});
 
   @override
   ProfileSwipeScreenState createState() => ProfileSwipeScreenState();
 }
 
 class ProfileSwipeScreenState extends State<SwipeScreen> {
-
   @override
   Widget build(BuildContext context) {
     final viewModel = context.watch<SwipeViewModel>();
     return FutureBuilder<List<Userprofile>>(
-        future:viewModel.profilesFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Scaffold(
-              body: Center(child: CircularProgressIndicator()),
-            );
-          } else if (snapshot.hasError) {
-            return Scaffold(
-              appBar: AppBar(title: const Text("Error")),
-              body: Center(child: Text("Error: ${snapshot.error}")),
-            );
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return Scaffold(
-              appBar: AppBar(title: const Text("No Matches")),
-              body: const Center(
-                child: Text(
-                  "No more profiles to show!",
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                ),
-              ),
-            );
-          }
-
+      future: viewModel.profilesFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        } else if (snapshot.hasError) {
           return Scaffold(
-            resizeToAvoidBottomInset: false,
-            appBar: AppBar(
-              title: Text(viewModel.state.title ?? "Matches (0)"),
-              centerTitle: true,
-            ),
-            body: SizedBox(
-              height: MediaQuery.of(context).size.height,
-              width: MediaQuery.of(context).size.width,
-              child: SwipableStack(
-                controller: viewModel.controller,
-                itemCount: viewModel.profiles.length,
-                horizontalSwipeThreshold: 0.8,
-                onSwipeCompleted: (index, direction) {
-                  viewModel.handleSwipe(direction, context);
-                },
-                builder: (context, properties) {
-                  viewModel.checkSwipeDirection(properties.swipeProgress);
-                  return Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(12),
-                      boxShadow: [
-                        if (viewModel.state.shouldShowGlow)
-                          BoxShadow(
-                            color: properties.direction == SwipeDirection.right
-                                ? Colors.green.withOpacity(0.9)
-                                : properties.direction == SwipeDirection.left
-                                ? Colors.red.withOpacity(0.9)
-                                : Colors.transparent,
-                            blurRadius: 40,
-                            spreadRadius: 5,
-                          ),
-                      ],
-                    ),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(12),
-                      child: FlipCard(
-                        profile: viewModel.profiles[properties.index % viewModel.profiles.length],
-                      ),
-                    ),
-                  );
-                },
-                overlayBuilder: (context, properties) {
-                  if (properties.direction == SwipeDirection.right && viewModel.state.shouldShowGlow) {
-                    return Align(
-                      alignment: Alignment.topLeft,
-                      child: Padding(
-                        padding: const EdgeInsets.all(24),
-                        child: Text(
-                          'ACCEPT',
-                          style: const TextStyle(
-                            color: Colors.green,
-                            fontSize: 50,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    );
-                  } else if (properties.direction == SwipeDirection.left && viewModel.state.shouldShowGlow) {
-                    return Align(
-                      alignment: Alignment.topRight,
-                      child: Padding(
-                        padding: const EdgeInsets.all(24),
-                        child: Text(
-                          'DECLINE',
-                          style: const TextStyle(
-                            color: Colors.red,
-                            fontSize: 50,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    );
-                  }
-                  return const SizedBox.shrink();
-                },
-                swipeAssistDuration: const Duration(milliseconds: 200),
-                stackClipBehaviour: Clip.hardEdge,
-                allowVerticalSwipe: false,
+            appBar: AppBar(title: const Text("Error")),
+            body: Center(child: Text("Error: ${snapshot.error}")),
+          );
+        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          return Scaffold(
+            appBar: AppBar(title: const Text("No Matches")),
+            body: const Center(
+              child: Text(
+                "No more profiles to show!",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
             ),
           );
-        },
-      );
-  }
+        }
 
+        return Scaffold(
+          resizeToAvoidBottomInset: false,
+          appBar: AppBar(
+            title: Text(viewModel.state.title ?? "Matches (0)"),
+            centerTitle: true,
+          ),
+          body: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Column(
+                children: [
+                  Expanded(
+                    child: SwipableStack(
+                      controller: viewModel.controller,
+                      itemCount: viewModel.profiles.length,
+                      horizontalSwipeThreshold: 0.8,
+                      onSwipeCompleted: (index, direction) {
+                        viewModel.handleSwipe(direction, context);
+                      },
+                      builder: (context, properties) {
+                        viewModel.checkSwipeDirection(properties.swipeProgress);
+                        return Container(
+                          decoration: BoxDecoration(
+                            borderRadius: AppConstants.borderRadiusLarge,
+                            boxShadow: [
+                              if (viewModel.state.shouldShowGlow)
+                                BoxShadow(
+                                  color:
+                                      properties.direction ==
+                                              SwipeDirection.right
+                                          ? Colors.green.withOpacity(0.9)
+                                          : properties.direction ==
+                                              SwipeDirection.left
+                                          ? Colors.red.withOpacity(0.9)
+                                          : Colors.transparent,
+                                  blurRadius: 40,
+                                  spreadRadius: 5,
+                                ),
+                            ],
+                          ),
+                          child: ClipRRect(
+                            borderRadius: AppConstants.borderRadiusLarge,
+                            child: FlipCard(
+                              profile:
+                                  viewModel.profiles[properties.index %
+                                      viewModel.profiles.length],
+                            ),
+                          ),
+                        );
+                      },
+                      overlayBuilder: (context, properties) {
+                        if (properties.direction == SwipeDirection.right &&
+                            viewModel.state.shouldShowGlow) {
+                          return Align(
+                            alignment: Alignment.topLeft,
+                            child: Padding(
+                              padding: const EdgeInsets.all(24),
+                              child: Text(
+                                'ACCEPT',
+                                style: const TextStyle(
+                                  color: Colors.green,
+                                  fontSize: 50,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          );
+                        } else if (properties.direction ==
+                                SwipeDirection.left &&
+                            viewModel.state.shouldShowGlow) {
+                          return Align(
+                            alignment: Alignment.topRight,
+                            child: Padding(
+                              padding: const EdgeInsets.all(24),
+                              child: Text(
+                                'DECLINE',
+                                style: const TextStyle(
+                                  color: Colors.red,
+                                  fontSize: 50,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          );
+                        }
+                        return const SizedBox.shrink();
+                      },
+                      swipeAssistDuration: const Duration(milliseconds: 200),
+                      stackClipBehaviour: Clip.hardEdge,
+                      allowVerticalSwipe: false,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
 }
