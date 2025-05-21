27a862110a13055f7108eda78956d77f9a1c06c9
robin <robinhartmann@live.com>
2025-05-21 14:03:59 +0200

    fix pipeline - linter issuee

--- lib/ui/request/widgets/widget/notification_card.dart
+++ lib/ui/request/widgets/widget/notification_card.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:knowledgematch/domain/models/notification_data.dart';
 import 'package:knowledgematch/domain/models/request_date_data.dart';
 import 'package:knowledgematch/domain/models/search_criteria.dart';
