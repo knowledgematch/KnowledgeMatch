e974f72aa44d58424811a2025d33d34eab893c14
Baris Tulgar <baris.tulgar@students.fhnw.ch>
2025-05-27 15:30:45 +0200

    user can hide or show the password during login.

--- lib/ui/login/widgets/login_screen.dart
+++ lib/ui/login/widgets/login_screen.dart
@@ -4,19 +4,29 @@ import 'package:knowledgematch/ui/forgot_pw/view_model/forgot_pw_view_model.dart
 import 'package:knowledgematch/ui/forgot_pw/widgets/forgot_pw_screen.dart';
 import 'package:knowledgematch/ui/login/view_model/login_view_model.dart';
 import 'package:provider/provider.dart';
-
 import '../../core/themes/app_colors.dart';
 import '../../core/ui/custom_page.dart';
 import '../../create_profile/widgets/create_profile_screen.dart';
 import '../../two_fa/view_model/two_fa_view_model.dart';
 import '../../two_fa/widgets/two_fa_screen.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+class _LoginScreenState extends State<LoginScreen> {
+  bool _obscureText = true;
+  void _toggleVisibility() {
+    setState(() {
+      _obscureText = !_obscureText;
+    });
+  }
+  @override
   Widget build(BuildContext context) {
     var viewModel = context.watch<LoginViewModel>();
+
     return Scaffold(
       body: CustomPage(
         child: Padding(
@@ -45,8 +55,17 @@ class LoginScreen extends StatelessWidget {
                   SizedBox(height: 20),
                   TextField(
                     controller: viewModel.passwordController,
-                    decoration: InputDecoration(labelText: 'Password'),
-                    obscureText: true,
+                    obscureText: _obscureText,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          _obscureText ? Icons.visibility_off : Icons.visibility,
+                          color: AppColors.grey6Light,
+                        ),
+                        onPressed: _toggleVisibility,
+                      ),
+                    ),
                   ),
                   SizedBox(height: 20),
                   Align(
@@ -107,21 +126,25 @@ class LoginScreen extends StatelessWidget {
                                     builder: (_) => ChangeNotifierProvider(
                                       create: (_) => TwoFAViewModel(),
                                       child: TwoFAScreen(
-                                          email: viewModel.emailController.text),
+                                        email: viewModel.emailController.text,
+                                      ),
                                     ),
                                   ),
                                 );
                               } else {
                                 ScaffoldMessenger.of(context).showSnackBar(
                                   SnackBar(
-                                      content: Text(viewModel.state.errorMessage!)),
+                                    content: Text(viewModel.state.errorMessage!),
+                                  ),
                                 );
                               }
                             },
                             child: viewModel.state.isLoading
                                 ? const CircularProgressIndicator()
-                                : const Text('Login',
-                                style: TextStyle(color: AppColors.whiteLight)),
+                                : const Text(
+                              'Login',
+                              style: TextStyle(color: AppColors.whiteLight),
+                            ),
                           ),
                         ),
                       ],
@@ -135,5 +158,4 @@ class LoginScreen extends StatelessWidget {
       ),
     );
   }
-
 }
